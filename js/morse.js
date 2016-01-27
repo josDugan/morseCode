c20c7ec20369abe66a22c323187538c6d27189c9
joseph <joseph@Zipcoder06s-MacBook-Pro.local>
2016-01-27 18:42:13 -0500

    Add js Morse code file

---
+++ js/morse.js
@@ -0,0 +1,75 @@
+var morseCode = {
+	A: ".-   ",
+	B: "-... ",
+	C: "-.-. ",
+	D: "-..  ",
+	E: ".    ",
+	F: "..-. ",
+	G: "--.  ",
+	H: ".... ",
+	I: "..   ",
+	J: ".--- ",
+	K: "-.-  ",
+	L: ".-.. ",
+	M: "--   ",
+	N: "-.   ",
+	O: "---  ",
+	P: ".--. ",
+	Q: "--.- ",
+	R: ".-.  ",
+	S: "...  ",
+	T: "-    ",
+	U: "..-  ",
+	V: "...- ",
+	W: ".--  ",
+	X: "-..- ",
+	Y: "-.-- ",
+	Z: "--.. ",
+	'1': ".----",
+	'2': "..---",
+	'3': "...--",
+	'4': "....-",
+	'5': ".....",
+	'6': "-....",
+	'7': "--...",
+	'8': "---..",
+	'9': "----.",
+	'10': "-----",
+	" ": "  |  ",
+	".": ".-.-.-",
+	",": "--..--",
+	":": "---...",
+	"?": "..--..",
+	"'": ".----.",
+	"-": "-....-",
+	"/": "-....-",
+	"(": "-.--.-",
+	")": "-.--.-",
+	"\"": ".-..-.",
+	"@": ".--.-.",
+	"=": "-...-" 
+}
+
+var translator = new Object();
+
+console.log("This is a Morse code translator");
+translator.input = prompt("Enter some text to translate into Morse code");
+translator.output = "";
+translator.translate = function() {
+	var input = translator.input.toUpperCase();
+	input.trim();
+	var inputArray = input.split("");
+    for (var character of inputArray) {
+        translator.output += morseCode[character] + " ";
+    }
+
+    return  translator.output;
+};
+
+var toPage = translator.translate();
+//var toEnglishTranslator = toPage.replace(/\|/g, " ");
+console.log(toPage);
+//console.log(toEnglishTranslator);
+
+document.getElementById('result').innerHTML = toPage;
+
